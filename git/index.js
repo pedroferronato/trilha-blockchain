22b32733678ad56992b966383ded880f21cc6c69
pedroferronato <pedroluisfbar@gmail.com>
2021-12-13 07:39:59 -0400

    Adição do atributo de dificuldade ao modelo Blockchain

--- git/index.js
+++ git/index.js
@@ -6,5 +6,5 @@ blockchain.addBlock({ amount: 4 })
 blockchain.addBlock({ amount: 50 })
 
 console.log(blockchain.isValid()) // true
-blockchain.blocks[1].data.amount = 30000 // ataque malicioso
+blockchain.blocks[1].data.amount = 30000 
 console.log(blockchain.isValid()) // false
