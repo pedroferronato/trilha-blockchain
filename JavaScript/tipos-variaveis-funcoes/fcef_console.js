4c7f92e409f68e78a26210a7e3b9a010865cdd66
pedroferronato <pedroluisfbar@gmail.com>
2021-12-17 08:53:40 -0400

    Testes com console.error

---
+++ JavaScript/tipos-variaveis-funcoes/fcef_console.js
@@ -0,0 +1,7 @@
+console.log('===== Console.error String =====')
+console.log("deu erro");
+console.error("deu erro");
+
+console.log('===== Console.error Instância de Error =====')
+console.log("deu erro");
+console.error(new Error("deu erro"));
