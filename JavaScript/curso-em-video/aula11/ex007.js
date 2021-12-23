456fc64dfdb16508b40dd72b74f450359730814e
pedroferronato <pedroluisfbar@gmail.com>
2021-12-23 07:18:46 -0400

    Aula 11 - ex007.js Condicionais

---
+++ JavaScript/curso-em-video/aula11/ex007.js
@@ -0,0 +1,7 @@
+let velocidade = 59
+
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`)
+
+if (velocidade > 60) console.log('Você ultrapassou a valocidade permitida. MULTADO!')
+
+console.log('Dirija sempre usand cinto de segurança!')
