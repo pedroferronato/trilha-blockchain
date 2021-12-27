5e05035e6b7dfb4a0c3eb92e2f0357379ac7ddae
pedroferronato <pedroluisfbar@gmail.com>
2021-12-27 12:14:04 -0400

    Configuração da conexão com base de dados MySQL

--- Node/node-express/index.js
+++ Node/node-express/index.js
@@ -1,5 +1,14 @@
 const customExress = require('./config/custom-express')
+const conexao = require('./infra/conexao')
 
-const app = customExress()
+conexao.connect((erro) => {
+    if (erro) console.log(erro);
+    else { 
+        console.log('DB conectado com sucesso')
+        
+        const app = customExress()
+        
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+    }
+})
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
