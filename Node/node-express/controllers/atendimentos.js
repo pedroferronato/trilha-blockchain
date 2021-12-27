485a6b7ab038dc48748541542f0e9529618da1a2
pedroferronato <pedroluisfbar@gmail.com>
2021-12-27 11:40:31 -0400

    Adição de método POST aos atendimentose configuração de body-parser

--- Node/node-express/controllers/atendimentos.js
+++ Node/node-express/controllers/atendimentos.js
@@ -1,3 +1,8 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Rota de atendimentos'))
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body);
+        res.send('Atendimentos, POST')
+    })
 }
