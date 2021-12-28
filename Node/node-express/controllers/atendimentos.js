ec11f68e99325b0cef2aac34ce2a55fe216f4e55
pedroferronato <pedroluisfbar@gmail.com>
2021-12-28 08:21:47 -0400

    Tratamento de erros Client-side no registro de atendimentos

--- Node/node-express/controllers/atendimentos.js
+++ Node/node-express/controllers/atendimentos.js
@@ -4,12 +4,6 @@ module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Rota de atendimentos'))
 
     app.post('/atendimentos', (req, res) => {
-        try {
-            Atendimento.adicionar(req.body)
-        } catch (error) {
-            console.log(error);
-            return res.status(400).send(error)
-        }
-        return res.status(201).send('Atendimento registrado com sucesso')
+        Atendimento.adicionar(req.body, res)
     })
 }
--- Node/node-express/models/atendimentos.js
+++ Node/node-express/models/atendimentos.js
@@ -2,20 +2,45 @@ const moment = require('moment')
 const conexao = require('../infra/conexao')
 
 class Atendimento {
-    adicionar(atendimento) {
+    adicionar(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        const sql = 'INSERT INTO atendimentos SET ?'
         
+        const dataValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteValido = atendimento.cliente.length >= 5
 
-        conexao.query(sql, atendimentoDatado, (erro, result) => {
-            if (erro) {
-                console.log(`[ERRO DB - Atendimento criar] - ${erro}`);
-                throw new Error('Falha ao criar atendimento' + erro)
+        const validacoes = [
+            {
+                nome: 'data', 
+                valido: dataValida, 
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente', 
+                valido: clienteValido, 
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-            else console.log(result);
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+
+        const existemErros = erros.length
+
+        if (existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+    
+            const sql = 'INSERT INTO atendimentos SET ?'
+            
+            conexao.query(sql, atendimentoDatado, (erro, result) => {
+                if (erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(result)
+                };
+            })
+        }
     }
 }
 
