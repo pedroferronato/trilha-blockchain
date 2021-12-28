ed6288fbd6ade2b63a16e6b90d0978c10cf23a4e
pedroferronato <pedroluisfbar@gmail.com>
2021-12-28 11:08:23 -0400

    Registro de pet com URL para imagem

--- Node/node-express/arquivos/uploadDeArquivos.js
+++ Node/node-express/arquivos/uploadDeArquivos.js
@@ -1,5 +1,21 @@
 const fs = require('fs')
+const path = require('path')
 
-fs.createReadStream('./assets/salsicha.jpg') // Async image stream reading 
-    .pipe(fs.createWriteStream('./assets/salsicha-stream.jpg')) // Função a ser executada com base na ReadStream 
-    .on('finish', () => console.log('Imagem foi escrita com sucesso')) // Callback baseado em trigger ('Finish')
+module.exports = (caminho, arquivo, callback) => {
+    const tiposValidos = ['jpg', 'png', 'jpeg']
+    const tipo = path.extname(caminho)
+    const tipoInvalido = tiposValidos.indexOf(tipo.substring(1)) === -1
+
+    if (tipoInvalido) {
+        const erro = {erro: 'Tipo é inválido'}
+        callback(erro)
+    } else {
+        const novoCaminho = `./assets/images/${arquivo}${tipo}`
+    
+        fs.createReadStream(caminho) // Async image stream reading 
+            .pipe(fs.createWriteStream(novoCaminho)) // Função a ser executada com base na ReadStream 
+            .on('finish', () => callback(false, novoCaminho)) // Callback baseado em trigger ('Finish')
+    }
+
+
+}
--- Node/node-express/models/pets.js
+++ Node/node-express/models/pets.js
@@ -1,14 +1,23 @@
 const conexao = require('../infra/conexao')
+const upload = require('../arquivos/uploadDeArquivos')
 
 class Pet {
     adicionar(pet, res) {
         const sql = 'INSERT INTO pets SET ?'
 
-        conexao.query(sql, pet, erro => {
+        upload(pet.imagem, pet.nome, (erro, novoCaminho) => {
             if (erro) {
                 res.status(400).json(erro)
             } else {
-                res.status(201).json(pet)
+                const novoPet = {nome: pet.nome, imagem: novoCaminho}
+                
+                conexao.query(sql, novoPet, erro => {
+                    if (erro) {
+                        res.status(400).json(erro)
+                    } else {
+                        res.status(201).json(novoPet)
+                    }
+                })
             }
         })
     }
