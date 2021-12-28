b8657eb00a1e9c7ae38f7ba83e274dfd21adfe13
pedroferronato <pedroluisfbar@gmail.com>
2021-12-28 07:18:52 -0400

    Adição da construção da tabela atendimentos

--- Node/node-express/index.js
+++ Node/node-express/index.js
@@ -1,10 +1,13 @@
 const customExress = require('./config/custom-express')
 const conexao = require('./infra/conexao')
+const tabelas = require('./infra/tabelas')
 
 conexao.connect((erro) => {
     if (erro) console.log(erro);
     else { 
         console.log('DB conectado com sucesso')
+        
+        tabelas.init(conexao)
         
         const app = customExress()
         
