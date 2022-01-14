810f2f156e66f00c8318098ccb1fe5a5ab11939c
pedroferronato <pedroluisfbar@gmail.com>
2022-01-14 08:59:37 -0400

    feat(task-manager): Desenvolvimento da API task-manager
    Desenvolvimento completo da API task-manager, praticando conhecimentos
    anteriores, refatoração de controllers com middlewares de asyncWrap e
    errorHandling.

--- Node/03-task-manager/app.js
+++ Node/03-task-manager/app.js
@@ -1,1 +1,31 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+require('dotenv').config()
+
+// middleware
+app.use(express.static('./public'))
+app.use(express.json())
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+// routes
+app.use('/api/v1/tasks', tasks)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+
+        app.listen(port, console.log(`Executando servidor na porta ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
